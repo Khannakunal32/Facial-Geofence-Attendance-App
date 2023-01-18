31b50e2cd7faa7e33c6acd2a49432b7a9d881329
Khannakunal32 <khannakunal32@gmail.com>
2023-01-19 04:37:26 +0530

    Geofence 3 page defence system

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+
+import Geofence from "react-native-expo-geofence";
+import Trial from "./Trial";
+import TrialThree from "./TrialThree";
+import TrialTwo from "./TrialTwo";
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <ScrollView>
+    // <View style={styles.container} >
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <Button title="but" onPress={getByProximity}></Button>
+    //   <StatusBar style="auto" />
+    // </View>
+    // </ScrollView>
+
+    <ScrollView>
+      <Trial></Trial>
+       <TrialTwo></TrialTwo>
+       <TrialThree></TrialThree>
+      </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
